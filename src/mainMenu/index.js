d529954164b8906e7f7177fd3c1c0c0cf2ad1692
coder <coder@vmi227394.contaboserver.net>
2019-06-27 03:56:57 +0200

    Change drawer to SwipeableDrawer

--- src/mainMenu/index.js
+++ src/mainMenu/index.js
@@ -1,9 +1,8 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
 import SaveIcon from '@material-ui/icons/Save';
@@ -16,85 +15,90 @@ import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const drawerWidth = 160;
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme=>({
   root: {
     display: 'flex'
   },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
   appBar: {
-    marginLeft: drawerWidth,
     [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
+      width: "100%",
     },
   },
   menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
+    marginRight: theme.spacing(2)
   },
   toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
   activeStyle: {
     fontWeight: "bold",
-    backgroundColor: "#D3D3D3",
-    textDecoration: "none"
+    color: "#000000",
+    backgroundColor: "#E6E6E6"
+  },
+  listItem: {
+    fontWeight: "inherit",
+    color: "inherit",
+    backgroundColor: "inherit"
   },
   navLink: {
     textDecoration: "none"
-  }
+  },
+  list: {
+    width: 150,
+  },
+  fullList: {
+    width: 'auto',
+  },
 }));
 
-export default function ResponsiveDrawer(props) {
-  const { container, children } = props;
+export default function SwipeableTemporaryDrawer(props) {
   const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [state, setState] = React.useState({
+    open: false,
+  });
 
-  function handleDrawerToggle() {
-    setMobileOpen(!mobileOpen);
-  }
+  const toggleDrawer = event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-  const drawer = (
-    <Fragment>
+    setState({ ...state, open: !state.open });
+  };
+
+  const SideList = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer}
+      onKeyDown={toggleDrawer}
+    >
       <div className={classes.toolbar} />
       <Divider />
       <List>
         <NavLink exact to="/" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Home"}>
+          <ListItem button key={"Home"} className={classes.listItem}>
             <ListItemIcon><HomeIcon /></ListItemIcon>
             <ListItemText primary={"Home"} />
           </ListItem>
         </NavLink>
         <NavLink to="/search" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Search"}>
+          <ListItem button key={"Search"} className={classes.listItem}>
             <ListItemIcon><SearchIcon /></ListItemIcon>
             <ListItemText primary={"Search"} />
           </ListItem>
         </NavLink>
         <NavLink to="/save" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Save"}>
+          <ListItem button key={"Save"} className={classes.listItem}>
             <ListItemIcon><SaveIcon /></ListItemIcon>
             <ListItemText primary={"Save"} />
           </ListItem>
         </NavLink>
       </List>
-    </Fragment>
+    </div>
   );
 
   return (
@@ -106,7 +110,7 @@ export default function ResponsiveDrawer(props) {
             color="inherit"
             aria-label="Open drawer"
             edge="start"
-            onClick={handleDrawerToggle}
+            onClick={toggleDrawer}
             className={classes.menuButton}
           >
             <MenuIcon />
@@ -117,39 +121,17 @@ export default function ResponsiveDrawer(props) {
         </Toolbar>
       </AppBar>
       <nav className={classes.drawer} aria-label="App Tools">
-        {/* The implementation can be swapped with js to avoid SEO duplication of NavLinks. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
+        <SwipeableDrawer
+          open={state.open}
+          onClose={toggleDrawer}
+          onOpen={toggleDrawer}
+        >
+          <SideList />
+        </SwipeableDrawer>
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        {children}
+        {props.children}
       </main>
     </div>
   );
