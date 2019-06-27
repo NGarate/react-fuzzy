ebd4dbba0547248394355780e7c5fe33c4885ad3
coder <coder@vmi227394.contaboserver.net>
2019-06-27 17:47:01 +0200

    Configured Linter

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import Menu from "./components/Menu";
 import SearchComponent from "./components/Search";
 
 function Index() {
-  return <h2>Home</h2>;
+    return <h2>Home</h2>;
 }
 
 function Search() {
-  return <SearchComponent />
+    return <SearchComponent />;
 }
 
 function Save() {
-  return <h2>Save</h2>;
+    return <h2>Save</h2>;
 }
 
 function Draw404() {
-  return <h2>404</h2>;
+    return <h2>404</h2>;
 }
 
 function AppRouter() {
-  return (
-    <Router>
-      <Menu>
-        <Switch>
-            <Route path="/" exact component={Index} />
-            <Route path="/search/" component={Search} />
-            <Route path="/save/" component={Save} />
-            <Route component={Draw404} />
-        </Switch>
-      </Menu>       
-    </Router>
-  );
+    return (
+        <Router>
+            <Menu>
+                <Switch>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/search/" component={Search} />
+                    <Route path="/save/" component={Save} />
+                    <Route component={Draw404} />
+                </Switch>
+            </Menu>
+        </Router>
+    );
 }
 
 export default AppRouter;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,138 +1,169 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import HomeIcon from '@material-ui/icons/Home';
-import SaveIcon from '@material-ui/icons/Save';
-import SearchIcon from '@material-ui/icons/Search';
-import {NavLink} from "react-router-dom";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
+import SaveIcon from "@material-ui/icons/Save";
+import SearchIcon from "@material-ui/icons/Search";
+import { NavLink } from "react-router-dom";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme=>({
-  root: {
-    display: 'flex'
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: "100%",
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex"
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  activeStyle: {
-    fontWeight: "bold",
-    color: "#000000",
-    backgroundColor: "#D4D4D4"
-  },
-  listItem: {
-    fontWeight: "inherit",
-    color: "inherit",
-    backgroundColor: "inherit"
-  },
-  navLink: {
-    textDecoration: "none"
-  },
-  list: {
-    width: 150,
-  },
-  fullList: {
-    width: 'auto',
-  },
+    appBar: {
+        [theme.breakpoints.up("sm")]: {
+            width: "100%"
+        }
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3)
+    },
+    activeStyle: {
+        fontWeight: "bold",
+        color: "#000000",
+        backgroundColor: "#D4D4D4"
+    },
+    listItem: {
+        fontWeight: "inherit",
+        color: "inherit",
+        backgroundColor: "inherit"
+    },
+    navLink: {
+        textDecoration: "none"
+    },
+    list: {
+        width: 150
+    },
+    fullList: {
+        width: "auto"
+    }
 }));
 
 export default function Menu(props) {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    open: false,
-  });
+    const classes = useStyles();
+    const { children } = props;
+    const [state, setState] = React.useState({
+        open: false
+    });
 
-  const toggleDrawer = event => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
+    const toggleDrawer = event => {
+        if (
+            event &&
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
 
-    setState({ ...state, open: !state.open });
-  };
+        setState({ ...state, open: !state.open });
+    };
 
-  const SideList = () => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer}
-      onKeyDown={toggleDrawer}
-    >
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-        <NavLink exact to="/" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Home"} className={classes.listItem}>
-            <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary={"Home"} />
-          </ListItem>
-        </NavLink>
-        <NavLink to="/search" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Search"} className={classes.listItem}>
-            <ListItemIcon><SearchIcon /></ListItemIcon>
-            <ListItemText primary={"Search"} />
-          </ListItem>
-        </NavLink>
-        <NavLink to="/save" className={classes.navLink} activeClassName={classes.activeStyle}>
-          <ListItem button key={"Save"} className={classes.listItem}>
-            <ListItemIcon><SaveIcon /></ListItemIcon>
-            <ListItemText primary={"Save"} />
-          </ListItem>
-        </NavLink>
-      </List>
-    </div>
-  );
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            edge="start"
+    const SideList = () => (
+        <div
+            className={classes.list}
+            role="presentation"
             onClick={toggleDrawer}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            React Fuzzy Search
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <nav className={classes.drawer} aria-label="App Tools">
-        <SwipeableDrawer
-          open={state.open}
-          onClose={toggleDrawer}
-          onOpen={toggleDrawer}
+            onKeyDown={toggleDrawer}
         >
-          <SideList />
-        </SwipeableDrawer>
-      </nav>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        {props.children}
-      </main>
-    </div>
-  );
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+                <NavLink
+                    exact
+                    to="/"
+                    className={classes.navLink}
+                    activeClassName={classes.activeStyle}
+                >
+                    <ListItem button key="Home" className={classes.listItem}>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                </NavLink>
+                <NavLink
+                    to="/search"
+                    className={classes.navLink}
+                    activeClassName={classes.activeStyle}
+                >
+                    <ListItem button key="Search" className={classes.listItem}>
+                        <ListItemIcon>
+                            <SearchIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Search" />
+                    </ListItem>
+                </NavLink>
+                <NavLink
+                    to="/save"
+                    className={classes.navLink}
+                    activeClassName={classes.activeStyle}
+                >
+                    <ListItem button key="Save" className={classes.listItem}>
+                        <ListItemIcon>
+                            <SaveIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Save" />
+                    </ListItem>
+                </NavLink>
+            </List>
+        </div>
+    );
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="Open drawer"
+                        edge="start"
+                        onClick={toggleDrawer}
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        React Fuzzy Search
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <nav className={classes.drawer} aria-label="App Tools">
+                <SwipeableDrawer
+                    open={state.open}
+                    onClose={toggleDrawer}
+                    onOpen={toggleDrawer}
+                >
+                    <SideList />
+                </SwipeableDrawer>
+            </nav>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {children}
+            </main>
+        </div>
+    );
 }
+Menu.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]).isRequired
+};
